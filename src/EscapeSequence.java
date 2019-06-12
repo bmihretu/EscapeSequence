f03ab3d36cdcc4a5ab6f53fe5711160b9fb6e7b9
bereket <motta4259@gmail.com>
2019-06-12 16:47:50 -0400

    first commit

---
+++ src/EscapeSequence.java
@@ -0,0 +1,14 @@
+public class EscapeSequence {
+
+        public static void main(String[] args){
+            System.out.print("****** \n");
+            System.out.print("* \t " + "* \n");
+            System.out.print("* \t " + "* \n");
+            System.out.print("* \t " + "* \n");
+            System.out.print("* \t " + "* \n");
+            System.out.print("****** \n");
+
+
+        }
+    }
+
